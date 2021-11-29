a5f2344bdb54d0ebc05387f29925ec11e71d2a97
Rafael Melo <rsmelo_@hotmail.com>
2021-11-28 23:17:35 -0300

    fix: keep injecting on tab change

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,21 +1,28 @@
 const API_URL = 'https://randomuser.me/api/';
 
-const mainContent = document.getElementById('main');
-if (mainContent && mainContent.firstElementChild) {
-  
-  const root = document.createElement('div');
-  root.setAttribute('id', 'root')
-  
-  mainContent.insertBefore(root, mainContent.firstElementChild.nextSibling);
-  
-  const rootEl = document.getElementById('root');
-  
-  if(rootEl) {
-    fetch(API_URL)
-      .then(res => res.json())
-      .then(({ results }) => {
-        const [user] = results;
-        createCard(rootEl, user);
-      });
+function triggerInjection() {
+  const mainContent = document.getElementById('main');
+  if (mainContent && mainContent.firstElementChild) {
+    const root = document.createElement('div');
+    root.setAttribute('id', 'root')
+    
+    mainContent.insertBefore(root, mainContent.firstElementChild.nextSibling);
+    
+    const rootEl = document.getElementById('root');
+    
+    if(rootEl) {
+      fetch(API_URL)
+        .then(res => res.json())
+        .then(({ results }) => {
+          const [user] = results;
+          createCard(rootEl, user);
+        });
+    }
   }
 }
+
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.hasChanged) {
+    triggerInjection();
+  }
+});
